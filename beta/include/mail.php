03d4c6e8ea1182fafe4441b5aa8d929e82efb4c2
TommyJams <nikhilk218@gmail.com>
2013-08-18 15:32:46 +0530

    Email - 20

--- beta/include/mail.php
+++ beta/include/mail.php
@@ -29,7 +29,7 @@ $message = "
     require_once("../plugin/phpmailer/class.phpmailer.php");
     $mail = new PHPMailer();  // create a new object
 	$mail->IsSMTP(); // enable SMTP
-	$mail->SMTPDebug = 0;  // debugging: 1 = errors and messages, 2 = messages only
+	$mail->SMTPDebug = 1;  // debugging: 1 = errors and messages, 2 = messages only
 	$mail->SMTPAuth = true;  // authentication enabled
 	$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
 	$mail->Host = 'smtp.gmail.com';
