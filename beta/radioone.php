fd2e3ec4d20073260ee77a1062eec075610394c5
TommyJams <nikhilk218@gmail.com>
2013-09-10 21:16:02 +0530

    Radio One Upload 8 Sep: Megha Neehar

--- beta/radioone.php
+++ beta/radioone.php
@@ -18,11 +18,11 @@ session_start();
 
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-	<meta property="og:title" content="One Bengaluru One Music: Sonal Bhatt" />
+	<meta property="og:title" content="One Bengaluru One Music: Megha Neehar" />
 
 	<meta property="og:type" content="website" />
 
-	<meta property="og:image" content="http://tommyjams.com/beta/images/radioone/artists/sonalbhatt.jpg"/>
+	<meta property="og:image" content="http://tommyjams.com/beta/images/radioone/artists/meghaneehar.jpg"/>
 
 	<meta property="og:url" content="http://tommyjams.com/beta/radioone.php" />
 
